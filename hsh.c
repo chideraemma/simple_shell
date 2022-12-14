cee015644492711b9631985f8eddca5445899057
chideraemma <emmachidera278@gmail.com>
2022-12-14 05:39:38 -0800

    fix Betty check

--- hsh.c
+++ hsh.c
@@ -10,41 +10,43 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-    char buffer[BUFFER_SIZE];
-    pid_t pid;
-
-    (void)argc;
-    (void)argv;
-    while (1) {
-        /*Display prompt and wait for user input*/ 
-        printf("$ ");
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
-            /*Handle end of file condition (Ctrl+D)*/ 
-            printf("\n");
-            break;
-        }
-
-        /*Remove trailing newline character*/ 
-        buffer[strcspn(buffer, "\n")] = 0;
-
-        /*Fork a child process to execute the command*/ 
-        pid = fork();
-        if (pid == 0) {
-            /*Child process*/
-	if (execve(buffer, argv, envp) == -1) {
-                /*Handle error if executable cannot be found*/ 
-                perror(buffer);
-            }
-            exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            /*Handle error if fork fails*/ 
-            perror("fork");
-        } else {
-            /*Parent process*/ 
-            wait(NULL);
-        }
-    }
-
-    return EXIT_SUCCESS;
+	char buffer[BUFFER_SIZE];
+	pid_t pid;
+	(void)argc;
+	(void)argv;
+	while (1)
+	{
+		/*Display prompt and wait for user input*/
+		printf("$ ");
+		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+		{
+			/*Handle end of file condition (Ctrl+D)*/
+			printf("\n");
+			break;
+		}
+		/*Remove trailing newline character*/
+		buffer[strcspn(buffer, "\n")] = 0;
+		/*Fork a child process to execute the command*/
+		pid = fork();
+		if (pid == 0)
+		{
+			/*Child process*/
+			if (execve(buffer, argv, envp) == -1)
+			{
+				/*Handle error if executable cannot be found*/
+				perror(buffer);
+			}
+			exit(EXIT_FAILURE);
+		} else if (pid < 0)
+		{
+			/*Handle error if fork fails*/
+			perror("fork");
+		}
+		else
+		{
+			/*Parent process*/
+			wait(NULL);
+		}
+	}
+	return (EXIT_SUCCESS);
 }
-
