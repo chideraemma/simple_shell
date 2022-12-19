9b2b0b597c55bac6aa6177a5a0d66fe6d42cd562
Symplice BONI <bonisimplice@gmail.com>
2022-12-19 04:46:20 +0000

    simple shell

--- helpers.c
+++ helpers.c
@@ -7,17 +7,17 @@
  */
 char *_getenv(const char *name)
 {
-        int i, result;
+	int i, result;
 
-        for (i = 0; environ[i]; i++)
-        {
-                result = _PATHstrcmp(name, environ[i]);
-                if (result == 0)
-                {
-                        return (environ[i]);
-                }
-        }
-        return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		result = _PATHstrcmp(name, environ[i]);
+		if (result == 0)
+		{
+			return (environ[i]);
+		}
+	}
+	return (NULL);
 }
 /**
  * _env - prints the environ
@@ -26,11 +26,11 @@ char *_getenv(const char *name)
  */
 int _env(void)
 {
-        int i;
+	int i;
 
-        for (i = 0; environ[i]; i++)
-                _puts(environ[i]);
-        return (0);
+	for (i = 0; environ[i]; i++)
+		_puts(environ[i]);
+	return (0);
 }
 /**
  * _puts - prints a string
@@ -38,11 +38,11 @@ int _env(void)
  */
 void _puts(char *str)
 {
-        int c;
+	int c;
 
-        for (c = 0; str[c] != '\0'; c++)
-                _putchar(str[c]);
-        _putchar('\n');
+	for (c = 0; str[c] != '\0'; c++)
+		_putchar(str[c]);
+	_putchar('\n');
 }
 /**
  * _putchar - prints a character
@@ -52,5 +52,20 @@ void _puts(char *str)
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
+}
+/**
+ * _memset - fills memory with a constant byte
+ * @s: character array to fill
+ * @b: constant byte to fill with
+ * @n: how many bytes to fill
+ * Return: the pointer to the char array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
--- shell.c
+++ shell.c
@@ -1,53 +1,40 @@
 #include "main.h"
-
-#define BUFFER_SIZE 1024
-
 /**
- * main - A simple shell
- * @argc: main argument
- * @argv: second main argument
- * @envp: third main argument
- * Return: EXIT_SUCCESS if success or EXIT_FAILURE if failed
+ * main - runs the shell program
+ *
+ * Return: 0 on success
  */
-
-int main(int argc, char *argv[], char *envp[])
+int main(void)
 {
-	char buffer[BUFFER_SIZE];
-	pid_t pid;
-	(void)argc;
+	char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
+	char *PATH = NULL;
+	char **av = NULL;
+	int exitstatus = 0;
+
+	signal(SIGINT, SIG_IGN);
+	PATH = _getenv("PATH");
+	if (PATH == NULL)
+		return (-1);
 	while (1)
 	{
-		/*Display prompt and wait for user input*/
-		printf("$ ");
-		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+		av = NULL;
+		prompt();
+		buffer = _read();
+		if (*buffer != '\0')
 		{
-			/*Handle end of file condition (Ctrl+D)*/
-			printf("\n");
-			break;
-		}
-		/*Remove trailing newline character*/
-		buffer[strcspn(buffer, "\n")] = 0;
-		/*Fork a child process to execute the command*/
-		pid = fork();
-		if (pid == 0)
-		{
-			/*Child process*/
-			if (execve(buffer, argv, envp) == -1)
+			av = tokenize(buffer);
+			if (av == NULL)
 			{
-				/*Handle error if executable cannot be found*/
-				perror(buffer);
+				free(buffer);
+				continue;
 			}
-			exit(EXIT_FAILURE);
-		} else if (pid < 0)
-		{
-			/*Handle error if fork fails*/
-			perror("fork");
+			fullpathbuffer = _fullpathbuffer(av, PATH, copy);
+			if (checkbuiltins(av, buffer, exitstatus) != 0)
+				continue;
+			exitstatus = _forkprocess(av, buffer, fullpathbuffer);
 		}
 		else
-		{
-			/*Parent process*/
-			wait(NULL);
-		}
+			free(buffer);
 	}
-	return (EXIT_SUCCESS);
+	return (0);
 }
